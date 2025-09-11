d238da4283f1631d1d9f749f73fee66084199b7c
Marcos Vazquez <mmvazquez364@gmail.com>
2025-09-10 23:17:46 -0400

    Successfully joined strings

--- 05_joinStrings/joinStrings.js
+++ 05_joinStrings/joinStrings.js
@@ -8,10 +8,18 @@
 */
 
 // Add your code right below, good luck!
+// For step 2
+const firstName = "Carlos";
+const lastName = "Stevenson";
+const thisYear = 1965;
+const birthYear = 1947;
 
+// For step 3
+const fullName = firstName + " " + lastName;
+const age = thisYear - birthYear;
 
-
-
+const greeting = "Hello! My name is " + fullName + " and I am " + age + " years old.";
+console.log(greeting);
 
 // Do not change this
 module.exports = {
